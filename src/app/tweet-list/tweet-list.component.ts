0cf32ae89d26e40a0c565741ee61b6bde79aba4c
Dillon Titcomb <dillontitcomb@gmail.com>
2018-04-08 20:14:59 -0700

    make tweet-list database reliant and remove some placeholder tweet list code.

--- src/app/tweet-list/tweet-list.component.ts
+++ src/app/tweet-list/tweet-list.component.ts
@@ -1,13 +1,21 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { User } from '../models/user.model';
 import { Tweet } from '../models/tweet.model';
+import { TweetService } from '../tweet.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-tweet-list',
   templateUrl: './tweet-list.component.html',
-  styleUrls: ['./tweet-list.component.css']
+  styleUrls: ['./tweet-list.component.css'],
+	providers: [TweetService]
 })
-export class TweetListComponent {
-	@Input() childTweetList: Tweet[];
+export class TweetListComponent implements OnInit {
+	tweets: FirebaseListObservable<any[]>;
+	constructor(private tweetService: TweetService) {}
 	@Input() thisUser: User;
+
+	ngOnInit(){
+		this.tweets = this.tweetService.getTweets();
+	}
 }
