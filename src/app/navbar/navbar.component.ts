b3b0773fa91c0fc773d9f6d204eaa7daa150e611
Dillon <dillontitcomb@gmail.com>
2018-04-17 11:30:25 -0700

    add logout functionality.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { User } from '../models/user.model';
 import { Tweet } from '../models/tweet.model';
+import { currentUser } from '../currentUser';
 
 @Component({
   selector: 'app-navbar',
@@ -8,6 +9,13 @@ import { Tweet } from '../models/tweet.model';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  currentUser = currentUser;
 
-
+  logoutClick() {
+    currentUser.userName = "";
+    currentUser.password = "";
+    currentUser.displayName = "";
+    currentUser.handle = "";
+    currentUser.displayImg = "https://www.merriam-webster.com/assets/mw/images/article/art-wap-article-main/egg-3442-e1f6463624338504cd021bf23aef8441@1x.jpg"
+  }
 }
