7a83b381beccb02da46588e2d517e7fb6cf61536
Dillon <dillontitcomb@gmail.com>
2018-04-17 09:14:59 -0700

    change user model to take four arguments.

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -30,5 +30,5 @@ export class BrowseComponent implements OnInit  {
 	year: number = this.currentTime.getFullYear();
 	dateString: string = this.month + "/" + this.day + "/" + this.year;
 
-	currentUser: User = new User("Me", "@personUsingTwitter", "https://www.merriam-webster.com/assets/mw/images/article/art-wap-article-main/egg-3442-e1f6463624338504cd021bf23aef8441@1x.jpg");
+	currentUser: User = new User("username", "password", "Me", "@personUsingTwitter");
 }
